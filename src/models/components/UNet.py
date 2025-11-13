0694d0bfe5c84a2cbff015e368e3ee384f3d42d5
gdseabra <gdseabra@gmail.com>
2025-11-13 16:39:07 -0500

    reduce number of channels per layer by 50%

--- src/models/components/UNet.py
+++ src/models/components/UNet.py
@@ -89,17 +89,17 @@ class UNet(nn.Module):
         bilinear = True
         self.bilinear = bilinear
 
-        self.inc = (DoubleConv(self.n_channels, 64))
-        self.down1 = (Down(64, 128))
-        self.down2 = (Down(128, 256))
-        self.down3 = (Down(256, 512))
+        self.inc = (DoubleConv(self.n_channels, 32))
+        self.down1 = (Down(32, 64))
+        self.down2 = (Down(64, 128))
+        self.down3 = (Down(128, 256))
         factor = 2 if bilinear else 1
-        self.down4 = (Down(512, 1024 // factor))
-        self.up1 = (Up(1024, 512 // factor, bilinear))
-        self.up2 = (Up(512, 256 // factor, bilinear))
-        self.up3 = (Up(256, 128 // factor, bilinear))
-        self.up4 = (Up(128, 64, bilinear))
-        self.outc = (OutConv(64, out_ch))
+        self.down4 = (Down(256, 512 // factor))
+        self.up1 = (Up(512, 256 // factor, bilinear))
+        self.up2 = (Up(256, 128 // factor, bilinear))
+        self.up3 = (Up(128, 64 // factor, bilinear))
+        self.up4 = (Up(64, 32, bilinear))
+        self.outc = (OutConv(32, out_ch))
 
     def forward(self, x):
         x1 = self.inc(x)
